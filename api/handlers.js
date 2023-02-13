e76819acd933a2b615bc3ed60a81d76672d55b06
Fábio Mastelari <fabio.mastelari@gmail.com>
2023-02-13 14:28:19 -0300

    'Adicionado conteudo referente as aulas 6 e 7'

--- api/handlers.js
+++ api/handlers.js
@@ -1,6 +1,54 @@
-const { randomUUID } = require('crypto')
+const { MongoClient, ObjectId } = require('mongodb')
+const { pbkdf2Sync } = require('crypto')
 
-const previousResults = new Map()
+async function connectToDatabase () {
+  const client = new MongoClient(process.env.MONGODB_CONNECTIONSTRING)
+  const connection = await client.connect()
+  return connection.db(process.env.MONGODB_DB_NAME)
+}
+
+async function basicAuth(event) {
+  const { authorization } = event.headers
+  if (!authorization) {
+    return {
+      statusCode:401,
+      body: JSON.stringify({ error: 'Missing authorization header' })
+    }
+  }
+
+  const [type, credentials] = authorization.split(' ')
+
+  if (type !== 'Basic') {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Unsuported authorization type' })
+    }
+  }
+
+  const [username, password] = Buffer.from(credentials, 'base64').toString().split(':')
+
+  const hashedPass = pbkdf2Sync(password, process.env.salt, 100000, 64, 'sha512').toString('hex')
+
+  const client = await connectToDatabase()
+  const collection = await client.collection('users')
+  const user = await collection.findOne({
+    name: username,
+    password: hashedPass
+  })
+
+  if (!user) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Invalid credentials' })
+    }
+  }
+
+  return {
+    id: user._id,
+    username: user.name
+  }
+
+}
 
 function extractBody(event) {
   if (!event?.body) {
@@ -13,9 +61,11 @@ function extractBody(event) {
 }
 
 module.exports.sendResponse = async(event) => {
+  const authResult = await basicAuth(event)
+  if (authResult.statusCode === 401) return authResult
   const { name, answers } = extractBody(event)
   const correctQuestions = [3, 1, 0, 2]
-  const correctAnswers = answers.reduce((acc, answer, index) => {
+  const totalCorrectAnswers = answers.reduce((acc, answer, index) => {
     if (answer === correctQuestions[index]) {
       acc++
     }
@@ -24,20 +74,22 @@ module.exports.sendResponse = async(event) => {
 
   const result = {
     name,
-    correctAnswers,
+    answers,
+    totalCorrectAnswers,
     totalAnswers: answers.length
   }
 
-  const resultId = randomUUID()
-  previousResults.set(resultId, { response: {name, answers}, result })
-  
+  const client = await connectToDatabase()
+  const collection = await client.collection('results')
+  const { insertedId } = await collection.insertOne(result)
+
   return {
     statusCode: 201,
     body: JSON.stringify({
-      resultId,
+      resultId: insertedId,
       __hypermedia: {
         href: `/results.html`,
-        query: { id: resultId }
+        query: { id: insertedId }
       }
     }),
     headers: {
@@ -47,7 +99,14 @@ module.exports.sendResponse = async(event) => {
 }
 
 module.exports.getResponse = async(event) => {
-  const result = previousResults.get(event.pathParameters.id)
+  const authResult = await basicAuth(event)
+  if (authResult.statusCode === 401) return authResult
+  const client = await connectToDatabase()
+  const collection = await client.collection('results')
+  const result = await collection.findOne({
+    _id: new ObjectId(event.pathParameters.id)
+  })
+  
   if (!result) {
     return {
       statusCode: 404,
