921872ddbfe066e21f1fe6dec803b0e43158721e
Fábio Mastelari <fabio.mastelari@gmail.com>
2023-02-14 09:10:36 -0300

    Adicionados conteudos relacionados com aula 9

--- api/handlers.js
+++ api/handlers.js
@@ -1,5 +1,6 @@
 const { MongoClient, ObjectId } = require('mongodb')
 const { pbkdf2Sync } = require('crypto')
+const { sign, verify } = require('jsonwebtoken')
 
 let connectionInstance = null
 
@@ -12,7 +13,7 @@ async function connectToDatabase () {
   return connectionInstance
 }
 
-async function basicAuth(event) {
+async function authorize(event){
   const { authorization } = event.headers
   if (!authorization) {
     return {
@@ -21,19 +22,42 @@ async function basicAuth(event) {
     }
   }
 
-  const [type, credentials] = authorization.split(' ')
+  const [type, token] = authorization.split(' ')
 
-  if (type !== 'Basic') {
+  if (type !== 'Bearer' || !token) {
     return {
       statusCode: 401,
       body: JSON.stringify({ error: 'Unsuported authorization type' })
     }
   }
 
-  const [username, password] = Buffer.from(credentials, 'base64').toString().split(':')
+  const decodedToken = verify(token, process.env.JWT_SECRET, { audience: 'alura-serverless' })
+  
+  if (!decodedToken) {
+    return {
+      statusCode: 401,
+      body: JSON.stringify({ error: 'Invalid token' })
+    }
+  }
 
+  return decodedToken
+
+}
+
+function extractBody(event) {
+  if (!event?.body) {
+    return {
+      statusCode: 442,
+      body: JSON.stringify({ error: 'Missing body' })
+    }
+  }
+  return JSON.parse(event.body)
+}
+
+module.exports.login = async (event) => {
+  const {username, password} = extractBody(event)
   const hashedPass = pbkdf2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex')
-  console.log(hashedPass)
+
   const client = await connectToDatabase()
   const collection = await client.collection('users')
   const user = await collection.findOne({
@@ -48,25 +72,18 @@ async function basicAuth(event) {
     }
   }
 
+  const token = sign({ username, id: user._id}, process.env.JWT_SECRET, { expiresIn: '24h', audience: 'alura-serverless'})
   return {
-    id: user._id,
-    username: user.name
+    statusCode: 200,
+    headers: {
+      'Content-type': 'application/json'
+    },
+    body: JSON.stringify({ token })
   }
-
-}
-
-function extractBody(event) {
-  if (!event?.body) {
-    return {
-      statusCode: 442,
-      body: JSON.stringify({ error: 'Missing body' })
-    }
-  }
-  return JSON.parse(event.body)
 }
 
 module.exports.sendResponse = async(event) => {
-  const authResult = await basicAuth(event)
+  const authResult = await authorize(event)
   if (authResult.statusCode === 401) return authResult
   const { name, answers } = extractBody(event)
   const correctQuestions = [3, 1, 0, 2]
@@ -104,7 +121,7 @@ module.exports.sendResponse = async(event) => {
 }
 
 module.exports.getResponse = async(event) => {
-  const authResult = await basicAuth(event)
+  const authResult = await authorize(event)
   if (authResult.statusCode === 401) return authResult
   const client = await connectToDatabase()
   const collection = await client.collection('results')
